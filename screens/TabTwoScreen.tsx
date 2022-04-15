a247567facc0b836ec99d83d43c2bad9cd669485
Olimjon Nematov <macmini@Olimjons-Mac-mini.local>
2022-04-15 07:07:30 -0500

    initial commit

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,16 +1,46 @@
-import { StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
+import ViewStoreDetails from '../components/ViewStoreDetails';
 import { Text, View } from '../components/Themed';
 
 export default function TabTwoScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
-  );
+  const [showStoreDetails, setStoreDetails] = useState(false);
+  const [showDailySales, setShowDailySales] = useState(false);
+  const [showOptions, setShowOptions] = useState(true);
+
+
+  const ViewContent = 
+   <View style={styles.container}>
+    <Text style={styles.title}>Choose one of the 2 options</Text>
+    <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    <Button title='View store details'
+      onPress={()=>{
+        setShowDailySales(false); 
+        setShowOptions(false);
+        setStoreDetails(true);}
+        }/>
+    <Button title='View daily sales' 
+      onPress={()=>{
+        setShowDailySales(true);
+        setShowOptions(false);
+        setStoreDetails(false);}
+        }/>
+    <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+  </View>;
+  
+  return (<View>
+    <Button title='home' onPress={()=>{        
+        setShowDailySales(false);
+        setShowOptions(true);
+        setStoreDetails(false);}}/>
+    {showDailySales && dailySales}
+    {showStoreDetails && <ViewStoreDetails />}
+    {showOptions && ViewContent}
+  </View>);
+
+  ;
 }
 
 const styles = StyleSheet.create({
